267a3714d2a8360d5a39fe1b5a26c2921b3db77a
unknown <rajatobing03@gmail.com>
2026-09-04 20:31:52 +0700

    week01: (tugas 1) refactor variables to val and use string templates

--- src/oop_00000163690_RajaSalomoLumbanTobing_week01/CircleCalculator.kt
+++ src/oop_00000163690_RajaSalomoLumbanTobing_week01/CircleCalculator.kt
@@ -18,3 +18,28 @@ fun checkSize(area: Double) {
         println("This is a Big Circle")
     } else {
         println("This is a Small Circle")
+
+        ```kotlin
+        fun main(args: Array<String>) {
+            // Variable definition
+            val radius = 7.0
+            val pi = 3.14
+
+            // Calculation
+            val area = pi * radius * radius
+
+            // Output menggunakan String Template
+            println("Radius: $radius, Area: $area")
+
+            // Logic check
+            checkSize(area)
+        }
+
+        fun checkSize(area: Double) {
+            if (area > 100) {
+                println("This is a Big Circle")
+            } else {
+                println("This is a Small Circle")
+            }
+        }
+        ```
