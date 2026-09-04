2b743175cd14185a1bff0628d7ee8b3edceb4ba1
unknown <rajatobing03@gmail.com>
2026-09-04 20:52:37 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000163690_RajaSalomoLumbanTobing_week01/GameStore.kt
+++ src/oop_00000163690_RajaSalomoLumbanTobing_week01/GameStore.kt
@@ -13,3 +13,20 @@ fun main() {
     val gameTitle = "Elden Ring"
     val price = 750000
 }
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000) (price * 0.2).toInt() else (price * 0.1).toInt()
+
+fun printReceipt(title: String, finalPrice: Int) {
+    println("Judul Game  : $title")
+    println("Harga Akhir : Rp$finalPrice")
+}
+
+fun main() {
+    val gameTitle = "Elden Ring"
+    val price = 750000
+
+    val discount = calculateDiscount(price)
+    val finalPrice = price - discount
+
+    printReceipt(title = gameTitle, finalPrice = finalPrice)
+}
