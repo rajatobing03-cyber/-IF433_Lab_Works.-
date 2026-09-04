9abf5991906fa97ec64c2a4026b6d574e5113eaf
unknown <rajatobing03@gmail.com>
2026-09-04 19:49:07 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000163690_RajaSalomoLumbanTobing_week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_00000163690_RajaSalomoLumbanTobing_week01
+
+fun main() {
+    var name: String = "Raja Salomo"
+    var score: int = 80
+
+    println("Nama: " +name + ", Nilai: " + score)
+
+}
