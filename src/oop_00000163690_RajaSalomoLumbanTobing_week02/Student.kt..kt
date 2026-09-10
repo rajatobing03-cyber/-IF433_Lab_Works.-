a6713141e967ed7be5b0d2f5ded18156bcbb1871
unknown <rajatobing03@gmail.com>
2026-09-10 16:52:42 +0700

    week02: create basic Student class structure

---
+++ src/oop_00000163690_RajaSalomoLumbanTobing_week02/Student.kt..kt
@@ -0,0 +1,9 @@
+package oop_00000163690_RajaSalomoLumbanTobing_week02
+
+class `Student.kt.` (
+    val name: String,
+    val  nim: String,
+    var major: String
+){
+    // Body class kosong dulu
+}
