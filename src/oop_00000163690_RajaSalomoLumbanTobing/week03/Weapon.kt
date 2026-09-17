1121d561c2d6735c75db9bc8b70d2a7595a95599
unknown <rajatobing03@gmail.com>
2026-09-18 00:50:06 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_00000163690_RajaSalomoLumbanTobing/week03/Weapon.kt
+++ src/oop_00000163690_RajaSalomoLumbanTobing/week03/Weapon.kt
@@ -5,4 +5,18 @@ class Weapon(val name: String) {
         set(value) {
             if (value < 0) {
                 println("Peringatan: Damage tidak boleh negatif!")
+            } else if (value > 1000) {
+                println("Peringatan: Damage melebihi 1000! Dipatok ke 1000.")
+                field = 1000
+            } else {
+                field = value
             }
+        }
+
+    val tier: String
+        get() = when {
+            damage > 800 -> "Legendary"
+            damage > 500 -> "Epic"
+            else -> "Common"
+        }
+}
