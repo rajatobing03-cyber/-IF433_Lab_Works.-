504b3260696237136f251473873728b36daad4f6
unknown <rajatobing03@gmail.com>
2026-09-19 11:15:40 +0700

    week04: (task 1) test ElectricCar in main

--- src/week04/Main.kt
+++ src/week04/Main.kt
@@ -1,22 +1,33 @@
 package oop_00000163690_RajaSalomoLumbanTobing.week04
 
 fun main() {
+    println("--- Manager ---")
     val manager = Manager(
         name = "Budi",
         baseSalary = 5000000
     )
 
+    manager.work()
+    println("Bonus Manager: Rp${manager.calculateBonus()}")
+
+    println("\n--- Developer ---")
     val developer = Developer(
         name = "Andi",
         baseSalary = 5000000,
         programmingLanguage = "Kotlin"
     )
 
-    println("--- Manager ---")
-    manager.work()
-    println("Bonus Manager: Rp${manager.calculateBonus()}")
-
-    println("\n--- Developer ---")
     developer.work()
     println("Bonus Developer: Rp${developer.calculateBonus()}")
+
+    println("\n--- Electric Car ---")
+    val electricCar = ElectricCar(
+        brand = "Tesla",
+        numberOfDoors = 4,
+        batteryCapacity = 80
+    )
+
+    electricCar.accelerate()
+    electricCar.honk()
+    electricCar.openTrunk()
 }
