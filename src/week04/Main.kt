3a4bcb09e3e006bfc96408fe33e11b3fec2b250b
unknown <rajatobing03@gmail.com>
2026-09-19 11:13:49 +0700

    week04: (task 2) test Employee hierarchy in main

--- src/week04/Main.kt
+++ src/week04/Main.kt
@@ -1,23 +1,22 @@
 package oop_00000163690_RajaSalomoLumbanTobing.week04
 
 fun main() {
-    println("--- Testing Vehicle ---")
-    val generalVehicle = Vehicle(brand = "Sepeda Onthel")
-    generalVehicle.honk()
-    generalVehicle.accelerate()
-
-    println("\n--- Testing Car ---")
-    val myCar = Car(brand = "Toyota", numberOfDoors = 4)
-    myCar.openTrunk()
-    myCar.honk()
-    myCar.accelerate()
-
-    println("\n--- Testing Electric Car ---")
-    val electricCar = ElectricCar(
-        brand = "Tesla",
-        numberOfDoors = 4,
-        batteryCapacity = 80
+    val manager = Manager(
+        name = "Budi",
+        baseSalary = 5000000
     )
-    electricCar.honk()
-    electricCar.accelerate()
+
+    val developer = Developer(
+        name = "Andi",
+        baseSalary = 5000000,
+        programmingLanguage = "Kotlin"
+    )
+
+    println("--- Manager ---")
+    manager.work()
+    println("Bonus Manager: Rp${manager.calculateBonus()}")
+
+    println("\n--- Developer ---")
+    developer.work()
+    println("Bonus Developer: Rp${developer.calculateBonus()}")
 }
